8e1d89a414b1bc6367af137773e991856482d601
Diego S?nchez de Rivera <diego.sanchezderiveracordoba@gmail.com>
2015-03-22 17:48:35 +0100

    run scripts

---
+++ script.bat
@@ -0,0 +1,3 @@
+START "Mongodb" "C:\Program Files\MongoDB 2.6 Standard\bin\mongod.exe" --dbpath C:\Users\Diego\node\app2\data
+START "Mongodb serverside" "C:\Program Files\MongoDB 2.6 Standard\bin\mongod.exe" --dbpath C:\Users\Diego\node\subscriber\data --port 27018
+START "Mosquito" "C:\Program Files (x86)\mosquitto\mosquitto.exe"
